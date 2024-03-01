b07a8b7de40054656337bd6e0a0278fc13677dfc
ivano-uglik <ivano.uglik7@gmail.com>
2024-03-01 20:46:20 +0100

    shuffled questions

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -11,8 +11,22 @@ import { BarManagerService } from '../bar-manager.service';
   styleUrl: './question.component.css',
 })
 export class QuestionComponent {
-  questions = inject(QuestionProviderService);
+  injectQuestions = inject(QuestionProviderService);
+  callQuestions = this.injectQuestions.getQuestions();
+  questions = this.callQuestions;
   manager = inject(AppManagerService);
   barManager = inject(BarManagerService);
+  randomOrder = Math.random() >= 0.5;
+  // if(randomOrder === false ) put correct in position 1
+  // if(randomOrder === true ) put correct in position 2
+  // if(randomOrder === ??? ) put correct in position 3
   @Input() index: number = 0;
+  incorrectClick() {
+    this.barManager.addChaserDown();
+    this.manager.setQuestionView();
+  }
+  correctClick() {
+    this.manager.setQuestionView();
+    this.barManager.moveCandidateDown();
+  }
 }
